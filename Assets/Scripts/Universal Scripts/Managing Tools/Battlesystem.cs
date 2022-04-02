4b151670bdfc0f41a1f95de1d95a6e689772f27b
AyayaCalling <mxkropp00@gmail.com>
2022-04-02 14:54:27 +0200

    Implementation of Battlesys with Enums

--- Assets/Scripts/Universal Scripts/Managing Tools/Battlesystem.cs
+++ Assets/Scripts/Universal Scripts/Managing Tools/Battlesystem.cs
@@ -4,15 +4,84 @@ using UnityEngine;
 
 public class Battlesystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    enum BattleState
     {
-        
+        PlayerTurn, EnemyTurn, Won, Lost, None
+    }  
+
+    public Player player;
+    private BattleState state = BattleState.PlayerTurn;
+
+   /* public void Update()
+    {
+        switch(state) 
+        {
+            case BattleState.PlayerTurn:
+            Debug.Log("sdsd");
+            break;
+
+            default:
+            Debug.Log("unexpected error");
+            break;
+        }
+        DealDamageToPlayer(player, 1);
+    } */
+
+    //These methods change the current Battlestate.
+    #region StateChanges
+
+    public void ChangeStateToPlayerTurn()
+    {
+        state = BattleState.PlayerTurn;
+        player.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeStateToEnemyTurn()
     {
-        
+        state = BattleState.EnemyTurn;
+        player.SetActive(false);
     }
+
+    public void ChangeStateToWon()
+    {
+        state = BattleState.Won;
+        player.SetActive(false);
+    }
+
+    public void ChangeStateToLost()
+    {
+        state = BattleState.Lost;
+        player.SetActive(false);
+    }
+
+    public void ChangeStateToNone()
+    {
+        state = BattleState.None;
+        player.SetActive(false);
+    }
+
+    #endregion
+
+    //Button-Method for the End-Turn Button.
+    public void OnEndturn()
+    {
+        ChangeStateToEnemyTurn();
+    }
+
+    //These methods deal damage to an enemy or player.
+    public void DealDamageToEnemy(Enemy thisEnemy, int amount)
+    {
+        thisEnemy.DecCurrentHP(amount);
+        Debug.Log("Dealt " + amount + " damage to Enemy " + thisEnemy);
+    }
+
+    public void DealDamageToPlayer(Player thisPlayer, int amount)
+    {
+        thisPlayer.DecCurrentHP(amount);
+        Debug.Log("Dealt " + amount + " damage to Player " + thisPlayer);
+    }
+
+
+
+    
 }
