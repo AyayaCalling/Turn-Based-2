3a63f221f45bf475cc1cf556624b6333aed8ce37
AyayaCalling <mxkropp00@gmail.com>
2022-04-03 15:31:13 +0200

    Set/Get + Inc/Dec for different Damage Types

--- Assets/Scripts/Knight/Skills/Strike.cs
+++ Assets/Scripts/Knight/Skills/Strike.cs
@@ -11,8 +11,82 @@ public class Strike : Skill
     public TargettingSystem targetter;
     public Battlesystem battle;
 
+    //These variables store the damage values for different damage sources.
+    private int baseDamage = 10;
+    private int statDamage;
+    private int itemDamage;
+
     #endregion
 
+    //These methods set and get different values of "Strike".
+    #region Setter/Getter
+
+    public void SetBaseDamage(int dmg)
+    {
+        baseDamage = dmg;
+    }
+
+    public int GetBaseDamage()
+    {
+        return baseDamage;
+    }
+
+    public void SetStatDamage(int dmg)
+    {
+        statDamage = dmg;
+    }
+
+    public int GetStatDamage()
+    {
+        return statDamage;
+    }
+
+    public void SetItemDamage(int dmg)
+    {
+        itemDamage = dmg;
+    }
+
+    public int GetItemDamage()
+    {
+        return itemDamage;
+    }
+    #endregion
+
+    //These methods increase and decrease different values of "Strike"
+    #region  Inc/Dec
+
+    public void IncBaseDamage(int dmg)
+    {
+        baseDamage += dmg;
+    }
+
+    public void DecBaseDamage(int dmg)
+    {
+        baseDamage -= dmg;
+    }
+
+    public void IncStatDamage(int dmg)
+    {
+        statDamage += dmg;
+    }
+
+    public void DecStatDamage(int dmg)
+    {
+        statDamage -= dmg;
+    }
+
+    public void IncItemDamage(int dmg)
+    {
+        itemDamage += dmg;
+    }   
+
+    public void DecItemDamage(int dmg)
+    {
+        itemDamage -= dmg;
+    }
+    #endregion
+
+    //This method will trigger every action binded to the "Strike" skill of "Knight.
     #region UseStrikeMethod
 
     /* This Method simulates a Strike.
@@ -24,7 +98,10 @@ public class Strike : Skill
         {
             if(Player.GetCurrentMana() > 0 && Player.GetActive())
             {
-                battle.DealDamageToEnemy(targetter.target, 10);
+                int damage = Mathf.RoundToInt((GetBaseDamage() + GetStatDamage() + GetItemDamage()) * (1-Player.GetWeakness()));
+
+
+                battle.DealDamageToEnemy(targetter.target, damage);
 
                 Player.DecCurrentMana(GetManaCost());
 
@@ -40,3 +117,4 @@ public class Strike : Skill
     #endregion
 
 }
+
